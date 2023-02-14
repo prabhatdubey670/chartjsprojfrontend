1229f9073627582f45f0be46d70184d4f9affa13
dprabhat670 <111510783+dprabhat670@users.noreply.github.com>
2023-02-14 18:30:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useRef } from 'react'
+import Nav from './components/Nav'
+import Charts from './components/Charts'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Nav />
+      <Charts />
+    </div>
+  )
+}
+
+export default App
